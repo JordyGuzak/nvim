2a01bb452786a89a56daf668165590178afbef09
Jordy Guzak <jordy.guzak@siemens.com>
2023-04-25 19:45:52 +0200

    updated mason and colors

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,6 +2,6 @@ require('tokyonight').setup({
   disable_background = true
 })
 
-vim.cmd.colorscheme('tokyonight-moon')
+vim.cmd.colorscheme('tokyonight-storm')
 --vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 --vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,3 +1,7 @@
+require('mason').setup()
+require('mason-lspconfig').setup({
+  ensure_installed = { "lua_ls", "tsserver", "jsonls", "html" }
+})
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 -- Mappings.
@@ -19,22 +23,21 @@ local on_attach = function(client, bufnr)
   local bufopts = { noremap=true, silent=true, buffer=bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<space>wl', function()
     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
   end, bufopts)
-  vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-  vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-  vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
 end
 
-local pid = vim.fn.getpid()
 local lspconfig = require('lspconfig')
 local configs = require'lspconfig.configs'
 
@@ -42,94 +45,12 @@ local configs = require'lspconfig.configs'
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local omnisharp_bin = "C:/Users/jordy.guzak/AppData/Local/omnisharp-vim/omnisharp-roslyn/OmniSharp.exe"
-lspconfig.omnisharp.setup({
-    cmd = { omnisharp_bin, "--languageserver" , "--hostPID", tostring(pid) },
-    on_attach = on_attach,
-    capabilities = capabilities
-})
-
-lspconfig.gopls.setup({
-    cmd = { 'gopls' },
-    on_attach = on_attach,
-    capabilities = capabilities
-})
-
 lspconfig.tsserver.setup {
     on_attach = on_attach,
     capabilities = capabilities
 }
 
-lspconfig.astro.setup {}
-
-lspconfig.diagnosticls.setup {
+lspconfig.lua_ls.setup {
   on_attach = on_attach,
-  filetypes = { 'javascript', 'javascriptreact', 'json', 'typescript', 'typescriptreact', 'css', 'less', 'scss', 'pandoc' },
-  init_options = {
-    linters = {
-      eslint = {
-        command = 'eslint_d',
-        rootPatterns = { '.git' },
-        debounce = 100,
-        args = { '--stdin', '--stdin-filename', '%filepath', '--format', 'json' },
-        sourceName = 'eslint_d',
-        parseJson = {
-          errorsRoot = '[0].messages',
-          line = 'line',
-          column = 'column',
-          endLine = 'endLine',
-          endColumn = 'endColumn',
-          message = '[eslint] ${message} [${ruleId}]',
-          security = 'severity'
-        },
-        securities = {
-          [2] = 'error',
-          [1] = 'warning'
-        }
-      },
-    },
-    filetypes = {
-      javascript = 'eslint',
-      javascriptreact = 'eslint',
-      typescript = 'eslint',
-      typescriptreact = 'eslint',
-    },
-    formatters = {
-      eslint_d = {
-        command = 'eslint_d',
-        rootPatterns = { '.git' },
-        args = { '--stdin', '--stdin-filename', '%filename', '--fix-to-stdout' },
-        rootPatterns = { '.git' },
-      },
-      prettier = {
-        command = 'prettier_d_slim',
-        rootPatterns = { '.git' },
-        -- requiredFiles: { 'prettier.config.js' },
-        args = { '--stdin', '--stdin-filepath', '%filename' }
-      }
-    },
-    formatFiletypes = {
-      css = 'prettier',
-      javascript = 'prettier',
-      javascriptreact = 'prettier',
-      json = 'prettier',
-      scss = 'prettier',
-      less = 'prettier',
-      typescript = 'prettier',
-      typescriptreact = 'prettier',
-      json = 'prettier',
-    }
-  }
+  capabilities = capabilities
 }
-
-lspconfig.emmet_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { 'html', 'typescriptreact', 'javascriptreact', 'css', 'sass', 'scss', 'less' },
-})
-
-vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    update_in_insert = true,
-  }
-)
