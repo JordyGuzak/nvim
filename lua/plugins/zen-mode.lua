d2cf9e97bc859d304659126ec146cbf8b24d6588
Jordy Guzak <jordy.guzak@gmail.com>
2023-12-22 12:03:05 +0100

    config update

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,8 +1,7 @@
 return {
   'folke/zen-mode.nvim',
   config = function()
-    require('zen-mode').toggle({
-      vim.keymap.set('n', '<C-F11>', '<cmd>ZenMode<cr>')
-    })
+    require('zen-mode').setup()
+    vim.keymap.set('n', '<C-F11>', '<cmd>ZenMode<cr>')
   end
 }
